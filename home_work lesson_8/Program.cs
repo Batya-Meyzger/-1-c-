ad9b1516ed8bc90e3e611e2a8405c7e03238790b
Batya Metzger <batya3767@gmail.com>
2025-11-29 19:39:33 +0200

    Add files via upload

---
+++ home_work lesson_8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace home_work_lesson_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
